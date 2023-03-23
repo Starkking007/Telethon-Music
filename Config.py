7bb63fb1f33bdb87b2c9ca327ff152426f001eb0
Starkking007 <128275979+Starkking007@users.noreply.github.com>
2023-03-23 21:23:50 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,8 +3,8 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "6213538"))
     API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5731389253:AAFmU--7Ld-HPusJ-NPc5RaUXJr3maANb5c")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOJ8Bu7EMlqhbQndpyVqgl0E7mu8c87M4ZPxT3Kh-5mO5GvYD0qkXD9ysWyfuIMmNXi3oX4sk5SbTkCr5Y82C9XQSNnDHia2AjeGPbTd_KZpLf5g_-INBxPU7bb_d80bSfCpwQYDw9pje68761j-8U-S6oa4s-4jc0duPUuWSXHcBdf1kjV0IKZIvvPHo9Uh0usP6qXK3hYHOfL3Pfp-z8sAzcYX_FuifEaE15nhQdolHqn0-07T0FYrL8a-frI2tJ3oUvMhrUMEsRbYFqpT3V78fxxR_pOHEXq9mwQwsBu9_E5V6KFS3mvQ0hHms8s50jB7xBPO9XvzaJsh9di-9dnBO3iA=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
